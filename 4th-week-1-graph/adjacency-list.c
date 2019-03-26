5103418f9d6e978279f560d2dadb8da71752fff0
xecachan <mbin96@gmail.com>
2019-03-26 20:06:56 +0900

    first

---
+++ 4th-week-1-graph/adjacency-list.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define MAXNODE 17
+
+typedef struct _node {
+	int vertex;
+	struct _node *next;
+}node;
+
+node *GL[MAXNODE];
+
+
+
+
+
+
+int nameToInt(char c) {
+	return c - 'A';
+}
+
+char intToChar(int i) {
+	return i + 'A';
+}
+
+
+void printNext(node a){
+    printf("%c",intToChar(a.vertex));
+    if (a.next!=NULL){
+        printNext(*(a.next));
+    }
+}
+void printAdjList(node * a[], int *v) {
+	
+
+	for (int i = 0; i < *v; i++) {
+        printf("%3c",intToChar(i));
+
+        printf("%3c",intToChar((*a[i]).vertex));
+        if ((*a[i]).next!=NULL){
+            printNext(*((*a[i]).next));
+        }
+        printf("\n");
+	}
+    
+	
+}
+
+void inputAdjList(node * a[], int *v, int *e) {
+	char vertex[3];
+	int i, j;
+	node *t;
+	printf("\ninput numberof node and edge\n");// 
+	scanf("%d %d", v, e);//입력받기. 앞이 노드 뒤가 엣지.
+	for (i = 0; i < *v; i++) {
+		a[i] = NULL;
+	}
+
+	for (j = 0; j < *e; j++) {
+		printf("\ninput two nodes consist of edge >");
+		scanf("%s", vertex);
+		i = nameToInt(vertex[0]);
+		t = (node *)malloc(sizeof(node));
+		t->vertex = nameToInt(vertex[1]);
+		t->next = a[i];
+		a[i] = t;
+        printf("%c",intToChar((*a[i]).vertex));
+
+		i = nameToInt(vertex[1]);
+		t = (node *)malloc(sizeof(node));
+		t->vertex = nameToInt(vertex[0]);
+		t->next = a[i];
+		a[i] = t;
+	}
+}
+
+int main() {
+	int v, e;
+
+	inputAdjList(GL, &v, &e);
+	printAdjList(GL, &v);
+}
